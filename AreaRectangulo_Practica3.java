42d72180a56b985c7b03c949004c45d8a4c7d334
Anton1oC4astr0 <143725409+Anton1oC4astr0@users.noreply.github.com>
2023-09-07 09:53:40 -0500

    Add files via upload

---
+++ AreaRectangulo_Practica3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operaciones;
+
+/**
+ *
+ * @author labcca
+ */
+public class Practica2 {
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es: Jose Antonio Lopez Castro");
+        
+        int b = 20;
+        int h = 5;
+        
+        int areaRectangulo = b*h;
+        
+        System.out.println("El area del rectangulo es: " + areaRectangulo);
+    }
+}
---
+++ TablaDel5_Practica2.psc
@@ -0,0 +1,8 @@
+Proceso Tabla_del_5
+	Definir tabla,i Como Entero;
+	Escribir "Ingresar la tabla";
+	Leer tabla;
+	Para i <- 1 Hasta 10 Hacer
+		Escribir tabla," x ", i," = ", i*tabla;
+	FinPara
+FinProceso
